38345a0c8ee24b9af63f2bc166f70ccafee8c7a8
Kieran O'Rourke <kieranor@gmail.com>
2018-12-17 23:19:13 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+document.getElementById("nav01").innerHTML =
+    "<ul id='menu'>" +
+    "<li><a href='index.html'>Home</a></li>" +
+    "<li><a href='#'>Data</a></li>" +
+    "<li><a href='french.html'>French Reading</a></li>" +
+    "<li><a href='webDev.html'>WebDev Links</a></li>" +
+    "<li><a href='french.html'>TBD</a></li>" +
+    "<li><a href='french.html'>TBD</a></li>" +
+    "</ul>";
+
+document.getElementById("foot01").innerHTML =
+     "<p>&copy;  " + new Date().getFullYear() + " Kieran O'Rourke. All rights reserved.</p>";
+
+
+
+
+
+
